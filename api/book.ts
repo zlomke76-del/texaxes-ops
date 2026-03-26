c40f6b50a06c4e0f91e4cd7032d26283a15b4d63
zlomke76-del <zlomke76@gmail.com>
2026-03-26 12:21:32 -0500

    Update book.ts

--- api/book.ts
+++ api/book.ts
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 import { createClient } from "@supabase/supabase-js";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-  apiVersion: "2024-06-20",
+  apiVersion: "2023-10-16",
 });
 
 const supabase = createClient(
